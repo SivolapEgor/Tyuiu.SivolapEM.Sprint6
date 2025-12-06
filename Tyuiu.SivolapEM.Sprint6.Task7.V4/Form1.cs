af667fe3ec6a6f8527d59e72435e2d78ebc39100
sbuw <esivolap1@gmail.com>
2025-12-06 23:45:46 +0500

    upd10

---
+++ Tyuiu.SivolapEM.Sprint6.Task7.V4/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.SivolapEM.Sprint6.Task7.V4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
