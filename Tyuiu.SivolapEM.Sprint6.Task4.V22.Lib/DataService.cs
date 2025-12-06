52cbcbfb7ce97d5259e17ae2e4cec4c41c144844
sbuw <esivolap1@gmail.com>
2025-12-06 23:28:37 +0500

    upd4task

---
+++ Tyuiu.SivolapEM.Sprint6.Task4.V22.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.SivolapEM.Sprint6.Task4.V22.Lib
+{
+    public class DataService : ISprint6Task4V22
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] res = new double[11];
+            int i = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (2 * x - 1 == 0)
+                {
+                    res[i] = 0;
+                    i++;
+                    continue;
+                }
+                res[i] = Math.Round(Math.Sin(x) + Math.Cos(2*x)/2 - 1.5 * x, 2);
+                i++;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SivolapEM.Sprint6.Task4.V22/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.SivolapEM.Sprint6.Task4.V22
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
