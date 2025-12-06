93b70122d5a3810e30724d57dda7ace4236f9be0
sbuw <esivolap1@gmail.com>
2025-12-06 23:41:13 +0500

    upd7

---
+++ Tyuiu.SivolapEM.Sprint6.Task6.V7.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.SivolapEM.Sprint6.Task6.V7.Lib
+{
+    public class DataService : ISprint6Task6V7
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string result = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+                string[] fragments;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    fragments = line.Split(' ');
+                    result += fragments.Length > 2 ? fragments[2] + " " : "";
+                }
+            }
+            result = result.Trim();
+            return result;
+        }
+    }
+}
